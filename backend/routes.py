418dab28de1b9b232e3b8274001ccbfedb34db74
Costas Constantinou <costas778@gmail.com>
2024-09-03 23:48:31 +0300

    Adding Health and Count routes to routes.py

--- backend/routes.py
+++ backend/routes.py
@@ -51,3 +51,51 @@ def parse_json(data):
 ######################################################################
 # INSERT CODE HERE
 ######################################################################
+######################################################################
+# Health Check Endpoint
+######################################################################
+@app.route("/health", methods=["GET"])
+def health():
+    """Return the health status of the server"""
+    return {"status": "OK"}
+
+######################################################################
+# Count Endpoint
+######################################################################
+@app.route("/count", methods=["GET"])
+def count():
+    """Return the count of songs in the database"""
+    count = db.songs.count_documents({})
+    return {"count": count}, 200
+
+######################################################################
+# Check connectivity to MongoDB
+######################################################################
+
+@app.route("/test-mongo", methods=["GET"])
+def test_mongo():
+    try:
+        # Fetch a single document from the songs collection to test the connection
+        song = db.songs.find_one()
+        if song:
+            return {"message": "Successfully connected to MongoDB!", "sample_song": parse_json(song)}, 200
+        else:
+            return {"message": "No data found in MongoDB!"}, 200
+    except Exception as e:
+        app.logger.error(f"Error connecting to MongoDB: {str(e)}")
+        return {"message": "Failed to connect to MongoDB"}, 500
+
+#MONGODB_SERVICE=localhost MONGODB_USERNAME=root MONGODB_PASSWORD=password flask run --reload --debugger
+
+#curl -X GET -i -w '\n' localhost:5000/test-mongo
+
+#OUTPUT#################################################################################################
+
+# HTTP/1.1 200 OK
+# Server: Werkzeug/2.1.2 Python/3.9.5
+# Date: Tue, 03 Sep 2024 20:03:13 GMT
+# Content-Type: application/json
+# Content-Length: 319
+# Connection: close
+
+# {"message":"Successfully connected to MongoDB!","sample_song":{"_id":{"$oid":"66d76b71db6d955ff9d2a5d4"},"id":1,"lyrics":"Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.","title":"duis faucibus accumsan odio curabitur convallis"}}
